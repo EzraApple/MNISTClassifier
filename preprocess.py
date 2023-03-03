ccc8f7d0cba80818525a46fcafaafd79b47faccf
Ezra Apple <ezraapple@berkeley.edu>
2023-03-03 13:45:19 -0800

    added normalize_features method

--- preprocess.py
+++ preprocess.py
@@ -7,5 +7,21 @@ def l2_normalize_samples(data):
     :param data: data
     :return: normalized data
     """
-    data = data/np.linalg.norm(data, axis=1)
+    data = data/np.linalg.norm(data, axis=1)[:, None]
+    return data
+
+
+def normalize_features(data):
+    """
+    subtracts mean from each feature in data (columns are features),
+    then divides by standard deviation. Each feature will now have mean 0,
+    and standard deviation 1
+    :param data: sample data
+    :return: normalized sample data
+    """
+    n, d = data.shape
+    feature_means = data.mean(axis=0)
+    feature_std = data.std(axis=0)
+    data = data - feature_means
+    data = data / feature_std
     return data
