ba28ccbfe0de0498c827b94a1c80ee2d660a1b58
Ezra Apple <ezraapple@berkeley.edu>
2023-03-04 14:33:35 -0800

    added regression.py + empty classes for various regression methods

---
+++ regression.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class Regression:
+    def fit(self, data, labels):
+        pass
+
+    def predict(self, data):
+        pass
+
+    def evaluate(self, data):
+        pass
+
+
+class LinearRegression:
+    pass
+
+
+class LogisticRegression:
+    pass
+
+
+class PolynomialRegression:
+    pass
+
+
+class RidgeRegression:
+    pass
+
+
+
+
