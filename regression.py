f33e084fff4d85dd7f8eb8dad4051d350c9c4a91
Ezra Apple <ezraapple@berkeley.edu>
2023-03-11 16:33:45 -0800

    started work on softmax regression

--- regression.py
+++ regression.py
@@ -1,32 +1,16 @@
 import numpy as np
 
 
-class Regression:
-    def fit(self, data, labels):
-        pass
+class SoftMaxRegression:
+
+    def fit(self, data, labels, iterations):
+        num_classes = np.unique(labels).shape[0]
+        n, d = data.shape
+        weights = np.zeros((data.shape[1], num_classes))
+
 
     def predict(self, data):
         pass
 
     def evaluate(self, data):
         pass
-
-
-class LinearRegression:
-    pass
-
-
-class LogisticRegression:
-    pass
-
-
-class PolynomialRegression:
-    pass
-
-
-class RidgeRegression:
-    pass
-
-
-
-
